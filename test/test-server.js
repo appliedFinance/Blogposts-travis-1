8b276330a146d262f1ac24eed504f8721bfabc9e
appliedFinance <atlusr@yahoo.com>
2018-06-06 11:47:33 -0400

    Straightened out the TEST_DATABASE require issue

--- test/test-server.js
+++ test/test-server.js
@@ -2,12 +2,13 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('BlogPost API on travis 1', function() {
-	before(function() { return runServer(); });
+	before(function() { return runServer(TEST_DATABASE_URL); });
 	after(function() { return closeServer(); });
 
 
