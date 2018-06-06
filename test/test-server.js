aee459050199397edd037f7597fb85293bbd7aff
appliedFinance <atlusr@yahoo.com>
2018-06-05 22:41:01 -0400

    Added dependencies in package.json

--- test/test-server.js
+++ test/test-server.js
@@ -1,7 +1,25 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
+const {app, runServer, closeServer} = require('../server');
+
 const expect = chai.expect;
 chai.use(chaiHttp);
 
+describe('BlogPost API on travis 1', function() {
+	before(function() { return runServer(); });
+	after(function() { return closeServer(); });
+
+
+	it('should list items on GET', function () {
+		return chai.request(app)
+			.get('/posts')
+			.then(function(result) {
+				expect(result).to.be.json;
+			});
+	});
+
+
+
+});
 
